351dcbe2b6be27b9c2c16935ffaa0ededa0f6fe8
konpeeyush <piyushksingh2589@gmail.com>
2023-03-17 21:56:26 +0530

    Adding a few changes

--- app.py
+++ app.py
@@ -7,6 +7,13 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/login', methods=['POST'])
+def login():
+    return render_template('login.html')
+
+@app.route('visualization')
+def visualization():
+    return render_template('visualization.html')
 
 if __name__ == "main":
     app.run(debug=True)
