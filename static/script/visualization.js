dd0a6831d4f8ed504e723b1897a5a7f807c9f47c
konpeeyush <piyushksingh2589@gmail.com>
2023-03-18 11:09:18 +0530

    Adding changes

--- static/script/visualization.js
+++ static/script/visualization.js
@@ -1,5 +1,20 @@
-import Chart from 'chart.js/auto';
-var ctx = document.getElementById('myChart').getContext('2d');
-var myChart = new Chart(ctx, {
-    
+const ctx = document.getElementById('myChart');
+
+new Chart(ctx, {
+    type: 'pie',
+    data: {
+        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+        datasets: [{
+            label: '# of Votes',
+            data: [12, 19, 3, 5, 2, 3],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        scales: {
+            y: {
+                beginAtZero: true
+            }
+        }
+    }
 });
